afc3cad0662bf7e0a0b1fb5c28a0c1d1d9b7ef66
vinidevel <vini_ide@yahoo.com.br>
2025-04-10 16:55:40 -0300

    Configurações gitpages

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/cosmic-canvas-voyage/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
